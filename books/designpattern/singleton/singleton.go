653ee669ec31630078685adcd1cfe67ca10b2c66
keito-isurugi <ja224kg@gmail.com>
2024-06-06 21:30:14 +0900

    singleton pattern

--- books/designpattern/singleton/singleton.go
+++ books/designpattern/singleton/singleton.go
@@ -5,16 +5,16 @@ import (
 	"sync"
 )
 
-type Singleton struct {
+type singleton struct {
 	value string
 }
 
-var singletonInstans *Singleton
+var singletonInstans *singleton
 var once sync.Once
 
-func GetInstance(s string) *Singleton {
+func GetInstance(s string) *singleton {
 	once.Do(func ()  {
-		singletonInstans = &Singleton{value: s}
+		singletonInstans = &singleton{value: s}
 	})
 	return singletonInstans
 }
@@ -25,6 +25,9 @@ func Exec() {
 
 	instance2 := GetInstance("Hello World!!!!!!!!")
 	fmt.Println(instance2.value)
+
+	instance3 := singleton{value: "hoge!"} // 同一パッケージ内だと複数生成できる。
+	fmt.Println(instance3.value)
 
 	if instance1 == instance2 {
 		fmt.Println("Both instances are the same")
