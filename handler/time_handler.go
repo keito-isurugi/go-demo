e0de35b3901dbd6778b875080ee1296d4117b9b5
keito-isurugi <ja224kg@gmail.com>
2024-10-29 21:53:04 +0900

    timedemoハンドラー作成

---
+++ handler/time_handler.go
@@ -0,0 +1,18 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/keito-isurugi/go-demo/demo"
+)
+
+func TimeDemoHandler(w http.ResponseWriter, r *http.Request) {
+	t := time.Date(2023, 12, 31, 0, 0, 0, 0, time.UTC)
+	addTwoMonth := demo.AddMonthsPreservingEndOfMonth(t, 2)
+	fmt.Println(t)
+	fmt.Println(addTwoMonth)
+
+	w.Write([]byte("demo of time"))
+}
