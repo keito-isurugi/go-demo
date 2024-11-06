4f21e39b273da549a6521d90d713b393520b0b21
keito-isurugi <ja224kg@gmail.com>
2024-11-06 21:12:10 +0900

    二分探索

--- demo/algorithm/algorithm.go
+++ demo/algorithm/algorithm.go
@@ -38,3 +38,21 @@ func BubbleDescSort(slice []int) []int {
 	}
 	return slice
 }
+
+func BinarySearch(arr []int, target int) int {
+	left, right := 0, len(arr) - 1
+
+	for left <= right {
+		mid := left + (right - left) / 2
+		
+		if arr[mid] == target {
+			return mid
+		} else if arr[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return -1
+}
