8202253c0d929281a8140d8b94da06a414c1a660
keito-isurugi <ja224kg@gmail.com>
2024-11-13 10:55:49 +0900

    goならわかるシステムプログラミング part6

--- books/system_programing_of_go/part6.go
+++ books/system_programing_of_go/part6.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"compress/gzip"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httputil"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -38,8 +42,8 @@ func part662() {
 
 		// POSTで文字列を送るリクエストを作成
 		request, err := http.NewRequest(
-			"POST", 
-			"http://localhost:8888", 
+			"POST",
+			"http://localhost:8888",
 			strings.NewReader(sendMessages[current]),
 		)
 		if err != nil {
@@ -114,15 +118,15 @@ func part661() {
 				}
 				fmt.Println(string(dump))
 				content := "Hello World!\n"
-				
+
 				//レスポンスを書き込む
 				// HTTP/1.1かつ、ContentLengthの設定が必要
 				response := http.Response{
-					StatusCode: 200,
-					ProtoMajor: 1,
-					ProtoMinor: 1,
+					StatusCode:    200,
+					ProtoMajor:    1,
+					ProtoMinor:    1,
 					ContentLength: int64(len(content)),
-					Body: io.NopCloser(strings.NewReader(content)),
+					Body:          io.NopCloser(strings.NewReader(content)),
 				}
 				response.Write(conn)
 			}
@@ -150,7 +154,6 @@ func part652() {
 	}
 	fmt.Println(string(dump))
 }
-
 
 func part651() {
 	listener, err := net.Listen("tcp", "localhost:8888")
@@ -181,10 +184,299 @@ func part651() {
 				StatusCode: 200,
 				ProtoMajor: 1,
 				ProtoMinor: 0,
-				Body: io.NopCloser(strings.NewReader("Hello World!\n")),
+				Body:       io.NopCloser(strings.NewReader("Hello World!\n")),
 			}
 			response.Write(conn)
 			conn.Close()
 		}()
+	}
+}
+
+// クライアントはgzipを受け入れ可能かチェック
+func isGZipAcceptable(request *http.Request) bool {
+	return strings.Index(strings.Join(request.Header["Accept-Encoding"], ","), "gzip") != -1
+}
+
+// 1セッションの処理をする
+func processSession(conn net.Conn) {
+	fmt.Printf("Accept %v\n", conn.RemoteAddr())
+	defer conn.Close()
+	for {
+		conn.SetReadDeadline(time.Now().Add(time.Second * 5))
+		// リクエストを読み込む
+		request, err := http.ReadRequest(bufio.NewReader(conn))
+		if err != nil {
+			neterr, ok := err.(net.Error)
+			if ok && neterr.Timeout() {
+				fmt.Println("timeout")
+				break
+			} else if err != io.EOF {
+				break
+			}
+		}
+		dump, err := httputil.DumpRequest(request, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+		// レスポンスを書き込む
+		response := &http.Response{
+			StatusCode: 200,
+			Proto:      "HTTP/1.1",
+			ProtoMajor: 1,
+			ProtoMinor: 1,
+			Header:     make(http.Header),
+		}
+		if isGZipAcceptable(request) {
+			content := "Hello World(gzipped)\n"
+			// コンテンツをgzip化して転送
+			var buffer bytes.Buffer
+			writer := gzip.NewWriter(&buffer)
+			io.WriteString(writer, content)
+			writer.Close()
+			response.Body = ioutil.NopCloser(&buffer)
+			response.ContentLength = int64(buffer.Len())
+			response.Header.Set("Content-Encoding", "gzip")
+		} else {
+			content := "Hello World\n"
+			response.Body = ioutil.NopCloser(strings.NewReader(content))
+			response.ContentLength = int64(len(content))
+		}
+		response.Write(conn)
+	}
+}
+
+func part61() {
+	listener, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go processSession2(conn)
+	}
+}
+
+// 青空文庫: ごんぎつねより
+// http://www.aozora.gr.jp/cards/000121/card628.html
+var contents = []string{
+	"これは、私わたしが小さいときに、村の茂平もへいというおじいさんからきいたお話です。",
+	"むかしは、私たちの村のちかくの、中山なかやまというところに小さなお城があって、",
+	"中山さまというおとのさまが、おられたそうです。",
+	"その中山から、少しはなれた山の中に、「ごん狐ぎつね」という狐がいました。",
+	"ごんは、一人ひとりぼっちの小狐で、しだの一ぱいしげった森の中に穴をほって住んでいました。",
+	"そして、夜でも昼でも、あたりの村へ出てきて、いたずらばかりしました。",
+}
+
+func processSession2(conn net.Conn) {
+	fmt.Printf("Accept %v\n", conn.RemoteAddr())
+	defer conn.Close()
+	for {
+		// リクエストを読み込む
+		request, err := http.ReadRequest(bufio.NewReader(conn))
+		if err != nil {
+			if err != io.EOF {
+				break
+			}
+			panic(err)
+		}
+		dump, err := httputil.DumpRequest(request, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+		// レスポンスを書き込む
+		fmt.Fprintf(conn, strings.Join([]string{
+			"HTTP/1.1 200 OK",
+			"Content-Type: text/plain; charset=utf-8",
+			"Transfer-Encoding: chunked",
+			"", "",
+		}, "\r\n"))
+		for _, content := range contents {
+			bynes := []byte(content)
+			fmt.Fprintf(conn, "%x\r\n%s\r\n", len(bynes), content)
+		}
+
+		fmt.Fprintf(conn, "0\r\n\r\n")
+	}
+}
+
+func part62() {
+	conn, err := net.Dial("tcp", "localhost:8888")
+	if err != nil {
+		panic(err)
+	}
+	request, err := http.NewRequest(
+		"GET",
+		"http://localhost:8888",
+		nil)
+	if err != nil {
+		panic(err)
+	}
+	err = request.Write(conn)
+	if err != nil {
+		panic(err)
+	}
+	reader := bufio.NewReader(conn)
+	response, err := http.ReadResponse(reader, request)
+	if err != nil {
+		panic(err)
+	}
+	dump, err := httputil.DumpResponse(response, false)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(dump))
+	if len(response.TransferEncoding) < 1 ||
+		response.TransferEncoding[0] != "chunked" {
+		panic("wrong transfer encoding")
+	}
+	for {
+		// サイズを取得
+		sizeStr, err := reader.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		// 16進数のサイズをパース。サイズがゼロならクローズ
+		size, err := strconv.ParseInt(
+			string(sizeStr[:len(sizeStr)-2]), 16, 64)
+		if size == 0 {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		// サイズ数分バッファを確保して読み込み
+		line := make([]byte, int(size))
+		reader.Read(line)
+		reader.Discard(2)
+		fmt.Printf("  %d bytes: %s\n", size, string(line))
+	}
+}
+
+// 順番に従ってconnに書き出しをする(goroutineで実行される)
+func writeToConn(sessionResponses chan chan *http.Response, conn net.Conn) {
+	defer conn.Close()
+	// 順番に取り出す
+	for sessionResponse := range sessionResponses {
+		// 選択された仕事が終わるまで待つ
+		response := <-sessionResponse
+		response.Write(conn)
+		close(sessionResponse)
+	}
+}
+
+// セッション内のリクエストを処理する
+func handleRequest(request *http.Request, resultReceiver chan *http.Response) {
+	dump, err := httputil.DumpRequest(request, true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(dump))
+	content := "Hello World\n"
+
+	// レスポンスを書き込む
+	// セッションを維持するためにKeep-Aliveでないといけない
+	response := &http.Response{
+		StatusCode:    200,
+		Proto:         "HTTP/1.1",
+		ProtoMajor:    1,
+		ProtoMinor:    1,
+		ContentLength: int64(len(content)),
+		Body:          ioutil.NopCloser(strings.NewReader(content)),
+	}
+
+	// 処理が終わったらチャネルに書き込み、
+	// ブロックされていたwriteToConnの処理を再始動する
+	resultReceiver <- response
+}
+
+func processSession3(conn net.Conn) {
+	fmt.Printf("Accept %v\n", conn.RemoteAddr())
+	// セッション内のリクエストを順に処理するためのチャネル
+	sessionResponses := make(chan chan *http.Response)
+	defer close(sessionResponses)
+	// レスポンスを直列化してソケットに書き出す専用のゴルーチン
+	go writeToConn(sessionResponses, conn)
+	reader := bufio.NewReader(conn)
+	for {
+		// レスポンスを受け取ってセッションのキューにいれる
+		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+		// リクエストを読み込む
+		request, err := http.ReadRequest(reader)
+		if err != nil {
+			neterr, ok := err.(net.Error)
+			if ok && neterr.Timeout() {
+				fmt.Println("Timeout")
+				break
+			} else if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+		sessionResponse := make(chan *http.Response)
+		sessionResponses <- sessionResponse
+		// 非同期でレスポンスを実行
+		go handleRequest(request, sessionResponse)
+	}
+}
+
+func part63() {
+	sendMessages := []string{
+		"ASCII",
+		"PROGRAMMING",
+		"PLUS",
+	}
+	current := 0
+	var conn net.Conn = nil
+	var err error
+	requests := make(chan *http.Request, len(sendMessages))
+	conn, err = net.Dial("tcp", "localhost:8888")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Access: %d\n", current)
+	defer conn.Close()
+	// リクエストだけ先に送る
+	for i := 0; i < len(sendMessages); i++ {
+		lastMessage := i == len(sendMessages)-1
+		request, err := http.NewRequest(
+			"GET",
+			"http://localhost:8888?message="+sendMessages[i],
+			nil)
+		if lastMessage {
+			request.Header.Add("Connection", "close")
+		} else {
+			request.Header.Add("Connection", "keep-alive")
+		}
+		if err != nil {
+			panic(err)
+		}
+		err = request.Write(conn)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("send: ", sendMessages[i])
+		requests <- request
+	}
+	close(requests)
+	// レスポンスをまとめて受信
+	reader := bufio.NewReader(conn)
+	for request := range requests {
+		response, err := http.ReadResponse(reader, request)
+		if err != nil {
+			panic(err)
+		}
+		dump, err := httputil.DumpResponse(response, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+		if current == len(sendMessages) {
+			break
+		}
 	}
 }
