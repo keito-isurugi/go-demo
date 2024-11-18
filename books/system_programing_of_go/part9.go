0caa8057dff19003b2b482332f469949b8991417
keito-isurugi <ja224kg@gmail.com>
2024-11-19 00:28:31 +0900

    goならわかるシステムプログラミング part9 途中

---
+++ books/system_programing_of_go/part9.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func Part9() {
+	part921()
+}
+
+func part921() {
+	f, _ := os.Create("file.txt")
+	a := time.Now()
+
+	f.Write([]byte("緑の怪獣"))
+	b := time.Now()
+
+	f.Sync()
+	c := time.Now()
+	f.Close()
+	
+	d := time.Now()
+	fmt.Printf("Write: %v\n", b.Sub(a))
+	fmt.Printf("Sync: %v\n", c.Sub(b))
+	fmt.Printf("Close: %v\n", d.Sub(c))
+}
