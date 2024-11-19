02be29dcd8559d252f6f282a3bb12ad9382fbaf3
keito-isurugi <ja224kg@gmail.com>
2024-11-19 21:19:21 +0900

    go fmt

--- demo/performance/performance_test.go
+++ demo/performance/performance_test.go
@@ -1,30 +1,21 @@
 package main
 
-import (
-    "testing"
-)
+import "testing"
 
 const size = 10000000
 
 // ベンチマーク関数: append の場合
 func BenchmarkAppend(b *testing.B) {
-    b.ReportAllocs()  // メモリ割り当てをレポートするように設定
-    for i := 0; i < b.N; i++ {
-        var slice []int
-        for j := 0; j < size; j++ {
-            slice = append(slice, j)
-        }
-    }
+	b.ReportAllocs() // メモリ割り当てをレポートするように設定
+	for i := 0; i < b.N; i++ {
+		var slice []int
+		for j := 0; j < size; j++ {
+			slice = append(slice, j)
+		}
+	}
 }
 
 // ベンチマーク関数: slice[i] = int の場合
 func BenchmarkIndexAssignment(b *testing.B) {
-    b.ReportAllocs()  // メモリ割り当てをレポートするように設定
-
-    for i := 0; i < b.N; i++ {
-        slice := make([]int, size)
-        for j := 0; j < size; j++ {
-            slice[j] = j
-        }
-    }
+	b.ReportAllocs() // メモリ割り当てをレポートするように設定
 }
