7281b59134291585c55b872dd70a0005df88c881
keito-isurugi <ja224kg@gmail.com>
2024-11-21 22:08:13 +0900

    goならわかるシステムプログラミング part10 途中

--- books/system_programing_of_go/go.mod
+++ books/system_programing_of_go/go.mod
@@ -3,6 +3,7 @@ module system_programing_of_go
 go 1.23.0
 
 require (
+	github.com/edsrzf/mmap-go v1.2.0 // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 )
--- books/system_programing_of_go/part10.go
+++ books/system_programing_of_go/part10.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
+	"sync"
+	"syscall"
+	"time"
 
+	"github.com/edsrzf/mmap-go"
 	"github.com/fsnotify/fsnotify"
 )
 
 func Part10() {
-	part101()
+	part103()
 }
 
 func part101() {
@@ -55,5 +63,81 @@ func part101() {
 		log.Fatal(err)
 	}
 	<-done
+}
 
+type FileLock struct {
+	l  sync.Mutex
+	fd int
+}
+
+func NewFileLock(filename string) *FileLock {
+	if filename == "" {
+		panic("filename needed")
+	}
+	fd, err := syscall.Open(filename, syscall.O_CREAT|syscall.O_RDONLY, 0750)
+	if err != nil {
+		panic(err)
+	}
+	return &FileLock{fd: fd}
+}
+
+func (m *FileLock) Lock() {
+	m.l.Lock()
+	if err := syscall.Flock(m.fd, syscall.LOCK_EX); err != nil {
+		panic(err)
+	}
+}
+
+func (m *FileLock) UnLock() {
+	if err := syscall.Flock(m.fd, syscall.LOCK_UN); err != nil {
+		panic(err)
+	}
+	m.l.Unlock()
+}
+
+func part1022() {
+	file := NewFileLock("file.txt")
+	fmt.Println("try locking...")
+	file.Lock()
+	fmt.Println("locked!")
+	time.Sleep(15 * time.Second)
+	file.UnLock()
+	fmt.Println("unlock")
+}
+
+func part103() {
+	// テストデータを書き込み
+	testData := []byte("0123456789ABCDEF")
+	testPath := filepath.Join(os.TempDir(), "testdata")
+	err := ioutil.WriteFile(testPath, testData, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	// メモリにマッピング
+	f, err := os.OpenFile(testPath, os.O_RDWR, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	// mはmmap.MMap型で[]byteのエイリアス
+	m, err := mmap.Map(f, mmap.RDWR, 0)
+	if err != nil {
+		panic(err)
+	}
+	defer m.Unmap()
+
+	// メモリ上のデータを修正して書き込む
+	m[9] = 'X'
+	m.Flush()
+
+	// 読み込んでみる
+	fileData, err := ioutil.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("original: %s\n", testData)
+	fmt.Printf("mmap:     %s\n", m)
+	fmt.Printf("file:     %s\n", fileData)
 }
