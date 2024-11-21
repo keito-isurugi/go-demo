14958c3bd45e0b59d4af3aaa3b5a19fff64f82b0
keito-isurugi <ja224kg@gmail.com>
2024-11-21 21:50:06 +0900

    スライスに要素追加のベンチマークテスト

--- demo/performance/performance_test.go
+++ demo/performance/performance_test.go
@@ -6,7 +6,7 @@ const size = 10000000
 
 // ベンチマーク関数: append の場合
 func BenchmarkAppend(b *testing.B) {
-	b.ReportAllocs() // メモリ割り当てをレポートするように設定
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		var slice []int
 		for j := 0; j < size; j++ {
@@ -17,5 +17,11 @@ func BenchmarkAppend(b *testing.B) {
 
 // ベンチマーク関数: slice[i] = int の場合
 func BenchmarkIndexAssignment(b *testing.B) {
-	b.ReportAllocs() // メモリ割り当てをレポートするように設定
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+        slice := make([]int, size)
+        for j := 0; j < size; j++ {
+            slice[j] = j
+        }
+    }
 }
