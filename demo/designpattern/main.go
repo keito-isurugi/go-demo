ed773af19b8caccbb2213f796b7093dc954f3624
keito-isurugi <ja224kg@gmail.com>
2025-02-19 21:00:00 +0900

    edit designpattern main

--- demo/designpattern/main.go
+++ demo/designpattern/main.go
@@ -1,13 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"designpattern/factory"
+	"designpattern/adapter"
+)
 
 func main() {
 	fmt.Println("Design Pattern")
 
 	fmt.Println("==========Strategy Pattern==========")
 	storategyExec()
-	
+
 	fmt.Println("==========Abstract Factory Pattern==========")
 	abstractFactoryExec()
+
+	fmt.Println("==========Singleton Pattern==========")
+	singletonExec()
+
+	fmt.Println("==========Factory Pattern==========")
+	factory.FactoryExec()
+
+	fmt.Println("==========Adaptor Pattern==========")
+	adapter.AdapterExec()
 }
