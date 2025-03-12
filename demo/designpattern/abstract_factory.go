25c77856fec44350c0b0500da43123a54b03ec4d
keito-isurugi <ja224kg@gmail.com>
2025-03-12 21:00:00 +0900

    add comment designpattern

--- demo/designpattern/abstract_factory.go
+++ demo/designpattern/abstract_factory.go
@@ -1,44 +1,61 @@
 package main
 
+// Notifier は通知を送るためのインターフェース
 type Notifier interface {
+	// SendNotification は通知メッセージを送信する
 	SendNotification(message string) string
 }
 
+// EmailNotifier は Email 通知の実装
 type EmailNotifier struct{}
 func (n *EmailNotifier) SendNotification(message string) string {
 	return "Email: " + message
 }
 
+// SMSNotifier は SMS 通知の実装
 type SMSNotifier struct{}
 func (n *SMSNotifier) SendNotification(message string) string {
 	return "SMS: " + message
 }
 
+// NotificationFactory は Notifier を作成するファクトリインターフェース
 type NotificationFactory interface {
+	// CreateNotifier は Notifier を作成する
 	CreateNotifier() Notifier
 }
 
+// EmailNotificationFactory は Email 通知のファクトリ
 type EmailNotificationFactory struct{}
 func (f *EmailNotificationFactory) CreateNotifier() Notifier {
 	return &EmailNotifier{}
 }
 
+// SMSNotificationFactory は SMS 通知のファクトリ
 type SMSNotificationFactory struct{}
 func (f *SMSNotificationFactory) CreateNotifier() Notifier {
 	return &SMSNotifier{}
 }
 
+// SendNotification はファクトリを使って通知を送信
 func SendNotification(factory NotificationFactory, message string) string {
 	notifier := factory.CreateNotifier()
 	return notifier.SendNotification(message)
 }
 
+// abstractFactoryExec は Abstract Factory パターンの使用例
 func abstractFactoryExec() {
+	// Email 通知の送信
 	emailFactory := &EmailNotificationFactory{}
 	emailMessage := SendNotification(emailFactory, "Hello, Email!")
 	println(emailMessage)
 
+	// SMS 通知の送信
 	smsFactory := &SMSNotificationFactory{}
 	smsMessage := SendNotification(smsFactory, "Hello, SMS!")
 	println(smsMessage)
 }
+
+
+// 出力
+// Email: Hello, Email!
+// SMS: Hello, SMS!
--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/keito-isurugi/go-demo
 
-go 1.22.0
+go 1.22.12
+
+toolchain go1.23.0
 
 require (
 	gorm.io/driver/postgres v1.5.7
@@ -8,7 +10,11 @@ require (
 )
 
 require (
+	github.com/99designs/gqlgen v0.17.66 // indirect
+	github.com/agnivade/levenshtein v1.2.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
@@ -16,9 +22,16 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sosodev/duration v1.3.1 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	github.com/urfave/cli/v2 v2.27.5 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.22 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
-	golang.org/x/sync v0.7.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/mod v0.23.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	golang.org/x/tools v0.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
