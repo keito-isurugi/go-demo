acd5a05c3b3a0e3a4479a9ff66b779df59992d55
keito-isurugi <ja224kg@gmail.com>
2025-03-17 21:00:00 +0900

    create rsademo

--- demo/crypto/rsa.go
+++ demo/crypto/rsa.go
@@ -16,6 +16,8 @@ func generateL() int {
 	return l
 }
 
+// - 1  < E < L
+// - gcd(E, L) = 1
 func generateE(l int) int {
 	e := 2
 	for e < l {
@@ -27,9 +29,10 @@ func generateE(l int) int {
 	return e
 }
 
+// 最大公約数
 func gcd(e, l int) int {
 	for l != 0 {
-		e, l = l, e%l
+		e, l = l, e % l
 	}
 	return e
 }
@@ -45,10 +48,36 @@ func generateD(e, l int) int {
 	return d
 }
 
+// m^exp mod n
+func modExp(m, exp, n int) int {
+	result := 1
+	for exp > 0 {
+		if exp%2 == 1 {
+			result = (result * m) % n
+		}
+		m = (m * m) % n
+		exp /= 2
+	}
+	return result
+}
+
 func execRSA() {
 	n := generateN()
 	fmt.Println("N:", n)
 
 	l := generateL()
 	fmt.Println("L:", l)
+
+	e := generateE(l)
+	fmt.Println("E:", e)
+
+	d := generateD(e, l)
+	fmt.Println("D:", d)
+
+	target := 8
+	fmt.Println("Target:", target)
+	c := modExp(target, e, n)
+	fmt.Println("C:", c)
+	d = modExp(c, d, n)
+	fmt.Println("D:", d)
 }
