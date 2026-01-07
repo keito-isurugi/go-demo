682add30fa3c2a4e6dc114730a61c21d31c50bb4
keito-isurugi <ja224kg@gmail.com>
2026-01-07 21:00:00 +0900

    create ddd domainservice

--- demo/ddd/domain/stock_allocation_service.go
+++ demo/ddd/domain/stock_allocation_service.go
@@ -1,43 +1,63 @@
 package domain
 
-// import "errors"
-
 type AllocatedItem struct {
-    ProductID ProductID
-    Quantity  int
+	ProductID ProductID
+	Quantity  int
 }
 
 type AllocationResult struct {
-	Success bool
+	Success        bool
 	AllocatedItems []AllocatedItem
-	FailedItems []ProductID
-	Message string
+	FailedItems    []ProductID
+	Message        string
 }
 
-func NewAllocationResult(order Order, stocs map[ProductID]*Stock) (AllocationResult, error) {
-	var success bool
-	var ai []AllocatedItem
-	var fi []ProductID
+type StockAllocationService struct{}
+
+func NewStockAllocationService() *StockAllocationService {
+	return &StockAllocationService{}
+}
+
+func (s *StockAllocationService) Allocate(order Order, stocks map[ProductID]*Stock) (AllocationResult, error) {
+	var allocatedItems []AllocatedItem
+	var failedItems []ProductID
 	var message string
 
-
-	for _, o := range order.lines {
-		if s, ok := stocs[o.ProductID()]; ok {
-			if !s.CanReserve(o.quantity.value) {
-				fi = append(fi, o.ProductID())
-
-				continue
-			}
-			s.Release(o.quantity.value)
-			// ai = append(ai, AllocatedItem{ProductID: o.ProductID(), Quantity: o.quantity.value})
+	for _, line := range order.lines {
+		stock, ok := stocks[line.ProductID()]
+		if !ok {
+			failedItems = append(failedItems, line.ProductID())
+			continue
 		}
+
+		if !stock.CanReserve(line.quantity.value) {
+			failedItems = append(failedItems, line.ProductID())
+			continue
+		}
+
+		newStock, err := stock.Reserve(line.quantity.value)
+		if err != nil {
+			failedItems = append(failedItems, line.ProductID())
+			continue
+		}
+
+		*stocks[line.ProductID()] = newStock
+		allocatedItems = append(allocatedItems, AllocatedItem{
+			ProductID: line.ProductID(),
+			Quantity:  line.quantity.value,
+		})
 	}
 
+	success := len(failedItems) == 0
+	message = ""
+	if !success {
+		message = "some items could not be allocated"
+	}
 
 	return AllocationResult{
-		Success: success,
-		AllocatedItems: ai,
-		FailedItems: fi,
-		Message: message,
+		Success:        success,
+		AllocatedItems: allocatedItems,
+		FailedItems:    failedItems,
+		Message:        message,
 	}, nil
 }
